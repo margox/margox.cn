35f174e00828fd94039c5ae65177e0dbdb8df723
柏舟 <wanggang@59store.com>
2016-10-17 17:31:30 +0800

    N/A

--- header.php
+++ header.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' );?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="format-detection" content="telephone=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="x5-page-mode" content="app">
+<meta name="x5-fullscreen" content="true">
+<meta charset="<?php bloginfo('charset');?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 <title><?php wp_title('|', true, 'right');?></title>
 <?php
