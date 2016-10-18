398b77af78eb8df3c30c8d8dca325c45f7c5c226
柏舟 <wanggang@59store.com>
2016-10-18 13:04:36 +0800

    N/A

--- footer.php
+++ footer.php
@@ -1,10 +1,7 @@
   <footer class="blog-footer">
     <h5 class="hello">该博客使用了激进的Web技术，请谨慎浏览</h5>
-    <div class="copyright">&copy; <?php echo date('Y');?> margox.cn</div>
+    <div class="copyright">&copy; <?php echo date('Y');?> margox.cn <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1260617397'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/stat.php%3Fid%3D1260617397%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script></div>
   </footer>
 <?php wp_footer();?>
-<div class="cnzz">
-  <script src="//s11.cnzz.com/stat.php?id=1260617397&web_id=1260617397" language="JavaScript"></script>
-</div>
 </body>
 </html>
