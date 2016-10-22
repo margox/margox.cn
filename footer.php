9960f01ebf23435b23b4ab81e704e60403abc160
柏舟 <wanggang@59store.com>
2016-10-22 21:20:24 +0800

    底部增加三个社交按钮

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
   <footer class="blog-footer">
-    <h5 class="hello">该博客使用了激进的Web技术，请谨慎浏览</h5>
+    <h5 class="hello">本站可能会使用激进的Web技术，请使用现代浏览器访问 ：）</h5>
+    <div class="social-icons">
+      <a href="http://weibo.com/margox" target="_blank" class="iconfont icon-weibo"></a>
+      <a href="https://github.com/margox/" target="_blank" class="iconfont icon-github"></a>
+      <a href="https://segmentfault.com/u/wanggang" target="_blank" class="iconfont icon-link"></a>
+    </div>
     <div class="copyright">&copy; <?php echo date('Y');?> margox.cn <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1260617397'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/stat.php%3Fid%3D1260617397%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script></div>
   </footer>
 <?php wp_footer();?>
